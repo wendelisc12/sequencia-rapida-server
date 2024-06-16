200ada177a99635c4c85c4f0d318f9da17ca678e
Wendel <wendel.isaac12@gmail.com>
2024-06-16 10:55:38 -0300

    criando get e post do ranking

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express")
+const {Pool} = require('pg')
+
+const pool = new Pool({
+    connectionString: `postgres://wbzqenwz:WnjHQkvBFgBVbsHOS8yEF_tEYx_-RRrs@silly.db.elephantsql.com/wbzqenwz`
+})
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+app.get("/", async(req, res)=>{
+    try {
+        const {rows} = await pool.query("SELECT * FROM rank ORDER BY pontos DESC");
+        return res.status(200).send(rows);
+    } catch (error) {
+        console.error('Rank indisponível', error);
+        return res.status(500).send('Erro interno do servidor');
+    }
+})
+
+app.post('/adicionar', async(req, res)=> {
+    try {
+        const { nome,pontos } = req.body;
+        const player = await pool.query(
+            'INSERT INTO rank (nome, pontos) VALUES ($1, $2) RETURNING *',
+            [nome, pontos]
+        );
+  
+        return res.status(201).json(player.rows[0]);
+    } catch (error) {
+        console.error('Erro ao adicionar player ao rank:', error);
+        return res.status(500).send('Erro interno do servidor');
+    }
+})
+
+
+app.listen (3000, ()=>{console.log('porta: 3000')})
